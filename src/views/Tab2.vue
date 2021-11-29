acc4b1289c13823d9f3594cd903d9090a248313e
William Koerich <william.koerich@ksys.com.br>
2021-11-29 16:00:24 -0300

    fix: fixed to show image in screen

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -15,7 +15,7 @@
       <ion-grid>
         <ion-row>
           <ion-col size="6" :key="photo" v-for="photo in photos">
-            <ion-img :src="photo.webviewPath"></ion-img>
+            <ion-img :src="photo.webPath"></ion-img>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -25,7 +25,6 @@
           <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button> 
       </ion-fab>
-      photos
     </ion-content>
 
     takePhoto
@@ -37,7 +36,7 @@ import { camera, trash, close } from 'ionicons/icons';
 import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon,
          IonToolbar, IonTitle, IonContent, IonGrid, IonRow,
          IonCol, IonImg } from '@ionic/vue';
-import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
 
 export default  {
   name: 'Tab2',
@@ -46,7 +45,6 @@ export default  {
          IonCol, IonImg },
   setup() {
     const { photos, takePhoto } = usePhotoGallery();
-
     return {
       photos,
       takePhoto,
